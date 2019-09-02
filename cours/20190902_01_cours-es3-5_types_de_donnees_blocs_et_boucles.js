bcacb10fdd9ed1a3236d642e36f058cda492d844
Sami Radi <sami.radi@virtuoworks.com>
2019-09-02 16:51:06 +0200

    Petite erreur sur le tableau corrigée

--- cours/20190902_01_cours-es3-5_types_de_donnees_blocs_et_boucles.js
+++ cours/20190902_01_cours-es3-5_types_de_donnees_blocs_et_boucles.js
@@ -1,6 +1,6 @@
 "use strict";
 
-["Berlin", ["Mur de Berlin", "Porte de Brandebourg", "Château de Charlottenburg"], "Paris", ["Notre-Dame, Tour-Eiffel", "Beaubourg", "Opera", "Palais du Luxembourg"], "Rome", ["Forum", "Colisée", "Chapelle Sixtine", "Pantheon"]];
+["Berlin", ["Mur de Berlin", "Porte de Brandebourg", "Château de Charlottenburg"], "Paris", ["Notre-Dame", "Tour-Eiffel", "Beaubourg", "Opera", "Palais du Luxembourg"], "Rome", ["Forum", "Colisée", "Chapelle Sixtine", "Pantheon"]];
 
 [40, 58, [478, 85, 745, 8], 74, [72, 14], [80, 741, 97]];
 
