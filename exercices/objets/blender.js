86dc22bd2a3ce58626f98aeb3137867e7ad354d8
Sami Radi <sami.radi@virtuoworks.com>
2019-09-04 15:48:13 +0200

    mer.  4 sept. 2019 15:48:13

--- exercices/objets/blender.js
+++ exercices/objets/blender.js
@@ -18,12 +18,12 @@ var blender = {
         };
     },
     mixerDesBooleens: function(x, y, z){
-        if("boolean" === typeof i && "boolean" === typeof j && "boolean" ===typeof k){
+        if("boolean" === typeof x && "boolean" === typeof y && "boolean" ===typeof z){
             var message = "";
             if(x && y && z){
                 message = "Tout est vrai !";
             }else{
-                message = "Au moins un argument n\"est pas vrai !";
+                message = "Au moins un argument n'est pas vrai !";
             };
             alert(message);
             return message;
@@ -45,7 +45,7 @@ var blender = {
     mixerDesObjets: function(o, t){
         if("object" === typeof o && "object" === typeof t){
             var p = (o.a * o.b) / (t.x * t.y);
-            alert("Si ceci : " + v + " est un nombre vous avez réussi !");
+            alert("Si ceci : " + p + " est un nombre vous avez réussi !");
             return p;
         }else{
             alert("Les types de données fournis sont incorrects !");
@@ -61,7 +61,7 @@ var blender = {
         };
     },
     mixerDeTout: function(k, l, m, n){
-        if("string" === typeof k && "object" === typeof l && "function" ===typeof m && "number" === typeof n){
+        if("string" === typeof k && "object" === typeof l && "function" === typeof m && "number" === typeof n){
             alert("Voici un message : " + k);
             alert("Voici un message trouvé dans un objet : " + l.message);
             m();
