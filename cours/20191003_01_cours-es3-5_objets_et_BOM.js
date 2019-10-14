b9f9882f10e59fe6ad8b7fc439d6d02c0f4efd5c
Sami Radi <sami.radi@virtuoworks.com>
2019-10-14 12:26:58 +0200

    Livret d'exercice terminé

--- cours/20191003_01_cours-es3-5_objets_et_BOM.js
+++ cours/20191003_01_cours-es3-5_objets_et_BOM.js
@@ -24,7 +24,7 @@ var autreTableauRetour = executerUneFonctionPourChaqueIndice(function(valeurFour
   return "La lettre " + valeurFournie;
 }, autreTableau); // => ["La lettre w", "La lettre x", "La lettre y", "La lettre z"]
 
-// Gestion des evenements :
+// Gestion des evenements avec .addEventListener()
 window.addEventListener("DOMContentLoaded", function(){
   var HTMLDivElements = window.document.getElementsByTagName("div");
   if (HTMLDivElements.length) {
@@ -32,12 +32,50 @@ window.addEventListener("DOMContentLoaded", function(){
     HTMLDivElements[0].addEventListener("click", function auClicSurLaDiv(evtSouris){
       // Instructions au clic sur la première div du document.
       // evtSouris sera un objet de type MouseEvent.
+      // Retirer un getionnaire d'évenement d'un élement.
+      HTMLDivElements[0].removeEventListener(auClicSurLaDiv);
     });
-
-    // Retirer un getionnaire d'évenement d'un élement.
-    HTMLDivElements[0].removeEventListener(auClicSurLaDiv);
   }
 
 });
 window.addEventListener("load", function(){});
 
+// $ et _ sont des nom de variables autorisés.
+var $;
+var _;
+
+// Définir le prototype à utiliser avec une fonction constructeur :
+var ConstructeurDeVoitures = function(){
+  this.xyz;
+}
+
+// ConstructeurDeVoitures.prototype = ???; // Ici on peut redéfinir entièrement le prototype à utiliser.
+
+// Ou enrichir le prototype par défaut (qui est un objet vide par défaut)
+/*
+ConstructeurDeVoitures.prototype.methode1 = function(){};
+ConstructeurDeVoitures.prototype.methode2 = function(){};
+ConstructeurDeVoitures.prototype.methode3 = function(){};
+*/
+
+var obj1 = new ConstructeurDeVoitures();
+
+// obj1.methode2(); // méthode du prototype
+
+// Ecrire un switch pour gérer les cas qui doivent aboutir à un résultat similaire
+var cas = "Twingo";
+
+switch(cas) {
+  case "Clio":
+  case "Twingo":
+  case "Megane":
+    window.alert("Renault");
+    break;
+  case "C3":
+  case "C4":
+  case "C5":
+    window.alert("Citroën");
+    break;
+  default:
+    window.alert("Constructeur inconnu !");
+}
